2566bb865557190734fa27a4df2c14f3628c8f9d
ahmed <ahmxd989@gmail.com>
2024-09-18 10:54:30 +0400

    wrapped in recoil root

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import "@/styles/globals.css";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+
+  return <RecoilRoot>
+            <Component {...pageProps} />;
+        </RecoilRoot>
 }
