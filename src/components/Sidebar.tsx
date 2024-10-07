22c733fee6bceccbf4d9e294663397477ce32627
ahmed <ahmxd989@gmail.com>
2024-10-07 15:16:44 +0400

    updated sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,85 +1,58 @@
 "use client";
-import React, { useState } from "react";
+import React, { ReactNode, useState } from "react";
 import { Sidebar, SidebarBody, SidebarLink } from "@/components/ui/sidebar";
-import {
-    IconArrowLeft,
-    IconBrandTabler,
-    IconSettings,
-    IconUserBolt,
-    
-} from "@tabler/icons-react";
+import {IconArrowLeft,IconBrandTabler,IconSettings,IconUserBolt,} from "@tabler/icons-react";
 import Link from "next/link";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle, CardFooter } from "@/components/ui/card";
-import {
-    AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogTrigger,
-} from "@/components/ui/alert-dialog"
-import { Button } from "./ui/button";
-import ChatComponent from "@/components/Chat";
-import { ProfileForm } from "@/components/Form";
-import GraphComponent from "./Graph";
-import DemoPage from "@/components/RequestFolder/page";
-import { useSetRecoilState, useRecoilValue } from "recoil"
-import { useRef } from "react";
-import { viewRequestAtom } from "@/store/atoms/viewRequest";
 
+interface LinkType {
+    label: string;
+    href: string;
+    icon: React.ReactNode;
+}
 
-export function SideBarDashboard() {
-    const links = [
-        {
-            label: "Dashboard",
-            href: "/home",
-            icon: (
-                <IconBrandTabler className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
-            ),
-        },
-        {
-            label: "Profile",
-            href: "/profile",
-            icon: (
-                <IconUserBolt className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
-            ),
-        },
-        {
-            label: "New Request",
-            href: "#",
-            icon: (
-                <IconUserBolt className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
-            ),
-        },
-        {
-            label: "Settings",
-            href: "#",
-            icon: (
-                <IconSettings className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
-            ),
-        },
-        {
-            label: "Logout",
-            href: "#",
-            icon: (
-                <IconArrowLeft className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
-            ),
-        },
-    ];
+const links: LinkType[] = [
+    {
+        label: "Dashboard",
+        href: "/home",
+        icon: <IconBrandTabler className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />,
+    },
+    {
+        label: "Profile",
+        href: "/profile",
+        icon: <IconUserBolt className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />,
+    },
+    {
+        label: "New Request",
+        href: "#",
+        icon: <IconUserBolt className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />,
+    },
+    {
+        label: "Settings",
+        href: "#",
+        icon: <IconSettings className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />,
+    },
+    {
+        label: "Logout",
+        href: "#",
+        icon: <IconArrowLeft className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />,
+    },
+];
+
+interface SideBarWrapperProps {
+    children: ReactNode;
+}
+
+export function SideBarWrapper({ children }: SideBarWrapperProps) {
     const [open, setOpen] = useState(false);
+
     return (
-        <div
-            className={cn(
-                "rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-screen flex-1 border border-neutral-200 dark:border-neutral-700 overflow-hidden",
-                "h-screen" // for your use case, use `h-screen` instead of `h-[60vh]`
-            )}
-        >
+        <div className={cn(
+            "rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-screen flex-1 border border-neutral-200 dark:border-neutral-700 overflow-hidden",
+            "h-screen"
+        )}>
             <Sidebar open={open} setOpen={setOpen}>
                 <SidebarBody className="justify-between gap-10">
                     <div className="flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
@@ -109,11 +82,14 @@ export function SideBarDashboard() {
                     </div>
                 </SidebarBody>
             </Sidebar>
-            <Dashboard />
+            <div className="flex flex-1">
+                    {children}
+            </div>
         </div>
     );
 }
-export const Logo = () => {
+
+const Logo: React.FC = () => {
     return (
         <Link
             href="#"
@@ -130,7 +106,8 @@ export const Logo = () => {
         </Link>
     );
 };
-export const LogoIcon = () => {
+
+const LogoIcon: React.FC = () => {
     return (
         <Link
             href="#"
@@ -141,61 +118,3 @@ export const LogoIcon = () => {
     );
 };
 
-// Dummy dashboard component with content
-const Dashboard = () => {
-    const viewRequest = useRecoilValue(viewRequestAtom);
-    return (
-        <div className="flex flex-1">
-            <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
-                {viewRequest ?
-                    (<GraphComponent></GraphComponent>)
-                    : (<main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
-                        {/* Main tag contains two div container, one for the request & leaderboard and another for chat  */}
-                        <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
-                            <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-4">
-                                <Card
-                                    className="sm:col-span-2 transition-all hover:scale-105" x-chunk="dashboard-05-chunk-0"
-                                >
-                                    <CardHeader className="pb-3">
-                                        <CardTitle className="mb-2">New Requests</CardTitle>
-                                        <CardDescription className="max-w-lg text-balance leading-relaxed">
-                                            Submit and manage your blood donation requests to connect promptly with available donors."
-                                        </CardDescription>
-                                    </CardHeader>
-                                    <CardFooter>
-                                        <Link href="/newrequest">
-                                        <Button variant="default" className="mt-2 transition-all hover:scale-110 border-gray-800">Make Request</Button>
-                                        </Link>
-                                    </CardFooter>
-                                </Card>
-                                <Card
-                                    className="sm:col-span-2 transition-all hover:scale-105" x-chunk="dashboard-05-chunk-0"
-                                >
-                                    <CardHeader className="pb-3">
-                                        <CardTitle className="mb-2">Past Requests</CardTitle>
-                                        <CardDescription className="max-w-lg text-balance leading-relaxed">
-                                            Submit and manage your blood donation requests to connect promptly with available donors."
-                                        </CardDescription>
-                                    </CardHeader>
-                                    <CardFooter>
-                                        <Link href="/profile">
-                                        <Button variant="outline" className="mt-2 transition-all hover:scale-110 border-gray-800">View Requests</Button>
-                                        </Link>
-                                    </CardFooter>
-                                </Card>
-
-                            </div>
-                            <Card className="p-4">
-                                <CardTitle> Track all your Requests...</CardTitle>
-                                <DemoPage ></DemoPage>
-                            </Card>
-                        </div>
-                        <div className="lg:grid-cols-1">
-
-                            <ChatComponent></ChatComponent>
-                        </div>
-                    </main>)}
-            </div>
-        </div>
-    );
-};
